a12293d7f4573c9fd9c51582f8bacb0ff6644e33
konovalov i <ivan.konovalov@noveogroup.com>
2019-09-16 10:18:56 +0700

    added connection actions and reducer

---
+++ src/actions/connection.js
@@ -0,0 +1,5 @@
+export const SET_CONNECTION_STATUS = 'SET_CONNECTION_STATUS'
+export const setConnectionStatus = (status) => ({
+  type: SET_CONNECTION_STATUS,
+  payload: status,
+})
---
+++ src/reducers/connection.js
@@ -0,0 +1,18 @@
+import { SET_CONNECTION_STATUS } from "../actions/connection"
+import { STATUS_OFFLINE, STATUS_ONLINE } from 'src/constants/connection';
+
+const initialState = {
+  status: navigator.onLine ? STATUS_ONLINE : STATUS_OFFLINE
+}
+
+export default function connection(state = initialState, action) {
+  switch (action.type) {
+    case SET_CONNECTION_STATUS:
+      return {
+        ...state,
+        status: action.payload
+      }
+    default:
+      return state
+  }
+}
