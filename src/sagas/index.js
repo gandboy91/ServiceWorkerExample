4ecf90c400e73d1ff102669827d51fb19e9f564b
konovalov i <ivan.konovalov@noveogroup.com>
2019-09-16 09:01:49 +0700

    added connection saga watcher

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,10 +1,12 @@
 import { all, call } from 'redux-saga/effects'
 import userWatcher from "./user"
 import cardsWatcher from "./cards"
+import connectionWatcher from "./connection"
 
 export default function* rootSaga() {
     yield all([
         call(userWatcher),
-        call(cardsWatcher)
+        call(cardsWatcher),
+        call(connectionWatcher)
     ])
 }
