158ace38ed8ea2b63e20de8ddf645cb2e1212291
konovalov i <ivan.konovalov@noveogroup.com>
2019-09-16 16:07:11 +0700

    refactored

--- src/swRegistrator.js
+++ src/swRegistrator.js
@@ -4,9 +4,6 @@ export function register() {
       navigator.serviceWorker.register('/sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        Notification.requestPermission().then(function(result) {
-          console.log('notifications mode: ', result);
-        });
       }, function(err) {
         // registration failed :(
         console.log('ServiceWorker registration failed: ', err.message);
