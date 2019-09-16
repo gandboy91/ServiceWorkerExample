f0f449c2cc2bb4b1cef38065708fa0c34043ebd7
konovalov i <ivan.konovalov@noveogroup.com>
2019-09-16 10:05:52 +0700

    updated bar

--- src/components/UserBar.js
+++ src/components/UserBar.js
@@ -1,32 +1,32 @@
-import React, { useCallback, useEffect } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSignOutAlt, faSignInAlt } from '@fortawesome/free-solid-svg-icons'
-import Spinner from "./common/Spinner";
-import {LoginRedirectButton, LogoutButton} from "./forms/buttons";
+import React, { useCallback, useEffect } from 'react';
+import {
+    ConnectionStatusButton,
+    LoginRedirectButton,
+    LogoutButton,
+} from './forms/buttons';
 
-const UserBar = React.memo(({ user: {name, id}, logoutRequest }) => {
-    const handleLogout = useCallback(() => logoutRequest(), [])
+const UserBar = React.memo(({ user: { name, id }, logoutRequest }) => {
+    const handleLogout = useCallback(() => logoutRequest(), []);
     const buttonProps = {
         style: { color: 'white' },
-        size: '2x'
-    }
+        size: '2x',
+    };
 
-    return <nav className="navbar navbar-dark bg-dark fixed-top">
-        <div className="navbar-header">
-            {
-                id
-                    ? <LogoutButton handleLogout={handleLogout} {...buttonProps} />
-                    : <LoginRedirectButton loginUrl='/login' {...buttonProps} />
-            }
-            <div className="navbar-brand">
-                {
-                    id
-                        ? `you logged in as ${name}`
-                        : `you haven't logged in`
-                }
+    return (
+        <nav className="navbar navbar-dark bg-dark fixed-top px-4">
+            <div className="navbar-item">
+                {id ? (
+                    <LogoutButton handleLogout={handleLogout} {...buttonProps} />
+                ) : (
+                    <LoginRedirectButton loginUrl="/login" {...buttonProps} />
+                )}
+                <span className="text-light">{id ? `you logged in as ${name}` : `you haven't logged in`}</span>
             </div>
-        </div>
-    </nav>
-})
+            <div className="navbar-brand">
+                <ConnectionStatusButton isOnline />
+            </div>
+        </nav>
+    );
+});
 
-export default UserBar
+export default UserBar;
