952ad2770075b4f6bb4c0d7a48235e9d715ea056
konovalov i <ivan.konovalov@noveogroup.com>
2019-09-18 15:43:49 +0700

    refactored

--- src/sagas/connection.js
+++ src/sagas/connection.js
@@ -1,37 +1,41 @@
-import { take, put, call } from 'redux-saga/effects'
-import { eventChannel } from 'redux-saga'
+import { take, select, put, call } from 'redux-saga/effects';
+import { eventChannel } from 'redux-saga';
 import { setConnectionStatus } from '../actions/connection';
 import { STATUS_OFFLINE, STATUS_ONLINE } from '../constants/connection';
 import { callWorker } from '../helpers/postMessage';
+import { selectOfflineStats } from 'src/selectors/cards';
 
 function connectionEventsChannel() {
-  return eventChannel(emit => {
-        const emitter = (event) => emit({
-          status: navigator.onLine ? STATUS_ONLINE : STATUS_OFFLINE
-        })
+  return eventChannel((emit) => {
+    const emitter = (event) =>
+        emit({
+          status: navigator.onLine ? STATUS_ONLINE : STATUS_OFFLINE,
+        });
 
-        window.addEventListener('online', emitter, false)
-        window.addEventListener('offline', emitter, false)
-        // The subscriber must return an unsubscribe function
-        return () => {
-          window.removeEventListener('online', emitter, false)
-          window.removeEventListener('offline', emitter, false)
-        }
-      }
-  )
+    window.addEventListener('online', emitter, false);
+    window.addEventListener('offline', emitter, false);
+    // The subscriber must return an unsubscribe function
+    return () => {
+      window.removeEventListener('online', emitter, false);
+      window.removeEventListener('offline', emitter, false);
+    };
+  });
 }
 
 function* connectionWatcher() {
-  const channel = yield call(connectionEventsChannel)
+  const channel = yield call(connectionEventsChannel);
   try {
     while (true) {
-      const { status } = yield take(channel)
-      yield call(callWorker, { a: 3, b: 2 })
-      yield put(setConnectionStatus(status))
+      const { status } = yield take(channel);
+      yield call(callWorker, {
+        type: status,
+        payload: status === STATUS_ONLINE ? yield select(selectOfflineStats) : {},
+      });
+      yield put(setConnectionStatus(status));
     }
   } catch (error) {
-    console.log('chanel error: ', error)
+    console.log('chanel error: ', error);
   }
 }
 
-export default connectionWatcher
+export default connectionWatcher;
