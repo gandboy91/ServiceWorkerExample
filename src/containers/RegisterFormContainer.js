981104cbe6abc0edf531965bc5047c13042fe13c
konovalov i <ivan.konovalov@noveogroup.com>
2019-10-18 16:45:12 +0700

    refactored

--- src/containers/RegisterFormContainer.js
+++ src/containers/RegisterFormContainer.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import { registerRequest } from "../actions/user";
-import RegisterForm from "../components/RegisterForm"
+import React from 'react';
+import { connect } from 'react-redux';
+import { registerRequest } from '../actions/user';
+import RegisterForm from '../components/RegisterForm';
 
 const mapStateToProps = ({ user: { errors, isProcessing } }) => ({
-    errors,
-    isProcessing
+  errors,
+  isProcessing,
 });
 
 const mapDispatchToProps = {
-    registerRequest
-}
+  registerRequest,
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(RegisterForm)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RegisterForm);
