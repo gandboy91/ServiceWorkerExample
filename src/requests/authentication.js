678242876ceed36397c8bba4b865a45133b1d6db
konovalov i <ivan.konovalov@noveogroup.com>
2019-10-21 13:14:14 +0700

    refactored

--- src/requests/authentication.js
+++ src/requests/authentication.js
@@ -4,4 +4,4 @@ export const login = ({ email, password }) =>
   post({ url: 'login', body: { email, password } });
 
 export const register = ({ email, password, name }) =>
-  post({ url: 'register', params: { email, password, name } });
+  post({ url: 'register', body: { email, password, name } });
